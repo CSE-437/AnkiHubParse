41a2cf630d8c9a60ef5873eae62e85640eb53838
David Ayeke <ayekedavid@yahoo.com>
2016-03-01 16:53:04 -0600

    Deck Transactions Done

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
-
-var api = new ParseServer({
+var options  = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337'  // Don't forget to change to https if needed
-});
+}
+var api = new ParseServer(options);
+
+console.log(options)
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
