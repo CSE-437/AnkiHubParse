2da8262d80c966cbb90224e000ef43ff6d2d5fad
David Ayeke <ayekedavid@yahoo.com>
2016-03-21 00:09:04 -0500

    Removed console

--- cloud/main.js
+++ cloud/main.js
@@ -75,16 +75,15 @@ Parse.Cloud.beforeSave("Card", function(req, res){
 });
 
 function ApplyTransactionToUser(t, user, errorCB, successCB){
-  console.log("here a", user);
+  
   switch(t.get('query')){
     case 'aDECK':
-      console.log('here 2.5',t.get('data'), t.get('data').gid);
-      console.log('here 2.6', user);
+
       if(!user.get('decks')){
         user.set('decks', []);
       }
       user.addUnique('decks', t.get('data').gid);
-      console.log('here 3');
+
     break;
 
     case 'rDECK':
@@ -110,10 +109,10 @@ function ApplyTransactionToUser(t, user, errorCB, successCB){
     break;
 
   }
-  console.log('here 5')
+  //console.log('here 5')
   user.save(null,{
-    success: function(){ console.log("here4"); successCB()},
-    error: function(user, error){ console.log('here6', arguments); errorCB(error)},
+    success: function(){ successCB()},
+    error: function(user, error){ errorCB(error)},
     sessionToken: user.get('sessionToken')
   });
 }
@@ -240,21 +239,21 @@ function ApplyTransactionToDeck(t, user, errorCB, successCB, res){
 
 Parse.Cloud.beforeSave("Transaction", function(req, res){
   //First validate Deck
-  console.log('here1');
+  //console.log('here1');
   var didParse = TUtil.ParseTransaction(req.object)
   if (didParse.error){
     return res.error(didParse.error)
   }
-  console.log('here1')
+  //console.log('here1')
   var t = didParse.transaction
   //Ensure req.object is the same
   req.object = t;
   var user = req.user;
-  console.log('here 1.5', user);
+  //console.log('here 1.5', user);
   if(!user){
     return res.error({error:"Need to be logged in to post transaction"});
   }else{
-    console.log('here b', user);
+    //console.log('here b', user);
   }
   switch(t.get("for")){
     case "User":
